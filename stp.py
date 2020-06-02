0dd95c37b14df2a21deca663bffcdb265eedaae4
Toda Hiroshi <hiroshitoda@ossaaaan.com>
2020-06-02 16:08:25 +0900

    Update stp.py

--- stp.py
+++ stp.py
@@ -2,4 +2,5 @@ from selenium import webdriver
 
 driver = webdriver.Safari(executable_path='/Applications/Safari Technology Preview.app/Contents/MacOS/safaridriver')
 driver.get('http://example.selenium.jp/reserveApp/')
+driver.save_screenshot("screenshots/exampleseleniumjp.png")
 driver.quit()
