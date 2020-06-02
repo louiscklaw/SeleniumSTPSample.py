a900bb227756cf66fe98496b340d820abea34cc8
Toda Hiroshi <hiroshitoda@ossaaaan.com>
2020-06-02 15:23:47 +0900

    Update s.py

--- s.py
+++ s.py
@@ -2,4 +2,5 @@ from selenium import webdriver
 
 driver = webdriver.Safari()
 driver.get('http://example.selenium.jp/reserveApp/')
+driver.save_screenshot("screenshots/exampleseleniumjp.png")
 driver.quit()
